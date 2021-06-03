1b39f88d2baaf0b479ee63dc6cba9a21eda9605a
FortyTwoFortyTwo <connorrowlanduk@gmail.com>
2021-06-03 13:53:52 +0100

    Add js function to easily set button color
    Removes any potential color class and set color to given value

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,18 @@ $(document).ready(function()
 	$(".memory-start").on("click", function()
 	{
 		//Set button color to grey
-		$(this).addClass("color-background-grey");
+		SetColor($(this), "grey");
 	});
 });
+
+function SetColor(btn, color)
+{
+	//Remove all class colors button may potentally have
+	btn.removeClass("color-background-red");
+	btn.removeClass("color-background-green");
+	btn.removeClass("color-background-blue");
+	btn.removeClass("color-background-grey");
+	
+	//Set button color to given value
+	btn.addClass("color-background-" + color);
+} 
